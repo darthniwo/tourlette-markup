f9e65ad171d7177a810dedc4f7c056a06657bcb7
Mauricio <mauricio.mejia@koombea.com>
2012-10-13 18:24:18 -0500

    added some STUFFS .... Zorro is your fault

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,5 +1,33 @@
 $(function(){
 	
+  var pos = 0;
+  setInterval(function(){
+    var position = pos + 'px 0px';
+    $('#pic-site').css('background-position', position);
+    //if(pos > -1920){
+      pos = pos - 10;
+    //}else{
+    //  pos = 0;
+    //}
+  },300);
+
+  //star rating plugin implementation with rating Off
+	$('.stars.no-rate').raty({
+		readOnly : true,
+		score    : 2,
+		path: "/assets/",
+		starOn  : "icons/starOn.png",
+		starOff : "icons/starOff.png"
+	});
+
+	//star rating plugin implementation with rating On
+	$('.stars.rate').raty({
+		readOnly : false,
+		score    : 2,
+		path: "/assets/",
+		starOn  : "icons/starOn.png",
+		starOff : "icons/starOff.png"
+	});
 
 	//#Modernizr fallback for placeholder on ie
 	 if(!Modernizr.input.placeholder){
